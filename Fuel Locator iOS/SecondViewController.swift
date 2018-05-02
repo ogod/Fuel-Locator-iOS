53124805d0b1360d5388c354b76b804b23e0e4be
Owen Godfrey <odgod@me.com>
2018-05-02 17:34:12 +0800

    Initial Commit

---
+++ Fuel Locator iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Fuel Locator iOS
+//
+//  Created by Owen Godfrey on 2/5/18.
+//  Copyright © 2018 Owen Godfrey. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
